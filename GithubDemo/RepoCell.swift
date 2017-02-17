2f7de33b2b06646f67ef607f8c414d272bfe8493
Hannah Lily Postman <postman.hl@gmail.com>
2017-02-17 00:04:40 -0800

    Basic cell layout, no auto layout yet

---
+++ GithubDemo/RepoCell.swift
@@ -0,0 +1,26 @@
+//
+//  RepoCell.swift
+//  GithubDemo
+//
+//  Created by Aristotle on 2017-02-16.
+//  Copyright © 2017 codepath. All rights reserved.
+//
+
+import UIKit
+
+class RepoCell: UITableViewCell {
+
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
