a59778de2f865221db634f8d8d293872911dd841
Hannah Lily Postman <postman.hl@gmail.com>
2017-02-17 01:27:38 -0800

    UI prototype cell modifications and according updates to cell class

--- GithubDemo/RepoCell.swift
+++ GithubDemo/RepoCell.swift
@@ -16,13 +16,31 @@ class RepoCell: UITableViewCell {
     @IBOutlet weak var repoAuthorLabel: UILabel!
     @IBOutlet weak var repoDescriptionLabel: UILabel!
     @IBOutlet weak var starsImageView: UIImageView!
-    
+    @IBOutlet weak var starCountLabel: UILabel!
     @IBOutlet weak var forksImageView: UIImageView!
-    
+    @IBOutlet weak var forkCountLabel: UILabel!
     @IBOutlet weak var avatarImageView: UIImageView!
+    
+    var repo: GithubRepo! {
+        didSet {
+            // Images
+            starsImageView.image = UIImage(named: "star")
+            forksImageView.image = UIImage(named: "fork")
+            avatarImageView.setImageWith(URL(string: repo.ownerAvatarURL!)!)
+            
+            // Labels
+            repoNameLabel.text = repo.name
+            repoAuthorLabel.text = repo.ownerHandle
+            repoDescriptionLabel.text = repo.description
+            starCountLabel.text = String(describing: repo.stars!)
+            forkCountLabel.text = String(describing: repo.forks!)
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
